61b6b4343cf158602f6c12bbb30a828e323f05f1
rachev3 <ddrachev123@gmail.com>
2024-02-22 13:13:48 +0200

    checkk

---
+++ PHONE-SERVICE/Migrations/20240222111324_checkk.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace PHONE_SERVICE.Migrations
+{
+    public partial class checkk : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
